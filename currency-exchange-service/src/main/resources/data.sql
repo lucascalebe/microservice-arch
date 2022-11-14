88be76a6ed2ea1b96139840829c67414cff3c98a
Lucas Calebe <lucascalebe07@hotmail.com>
2022-11-14 09:58:20 -0300

    :hammer: update script to BRL in db

--- currency-exchange-service/src/main/resources/data.sql
+++ currency-exchange-service/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment) values (10001, 'USD', 'INR', 65, '');
-INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment) values (10002, 'EUR', 'INR', 75, '');
-INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment) values (10003, 'AUD', 'INR', 25, '');
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment) values (10001, 'USD', 'BRL', 65, '');
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment) values (10002, 'EUR', 'BRL', 75, '');
+INSERT INTO currency_exchange (id, currency_from, currency_to, conversion_multiple, environment) values (10003, 'AUD', 'BRL', 25, '');
