a8534efa6be9019b83caa2b109fe5c7fbc190154
Lucas Calebe <lucascalebe07@hotmail.com>
2022-11-14 11:05:57 -0300

    :sparkles: invoking currency exchange from currency conversion with rest template

--- currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/controller/CurrencyConversionController.java
+++ currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/controller/CurrencyConversionController.java
@@ -1,12 +1,15 @@
 package com.example.microservices.currencyconversionservice.controller;
 
 import com.example.microservices.currencyconversionservice.CurrencyConversion;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
 
 @RestController
 @RequestMapping("currency-conversion")
@@ -15,6 +18,17 @@ public class CurrencyConversionController {
   @GetMapping("/from/{from}/to/{to}/quantity/{quantity}")
   public CurrencyConversion calculateCurrencyConversion(@PathVariable String from, @PathVariable String to,
                                                         @PathVariable BigDecimal quantity) {
-    return new CurrencyConversion(10001L, from, to, quantity, BigDecimal.TEN, BigDecimal.ONE, "");
+
+    HashMap<String, String> uriVariables = new HashMap<>();
+    uriVariables.put("from", from);
+    uriVariables.put("to", to);
+
+    ResponseEntity<CurrencyConversion> responseEntity = new RestTemplate().getForEntity("http://localhost:8000/currency-exchange/from/{from}/to/{to}",
+            CurrencyConversion.class, uriVariables);
+
+    CurrencyConversion currencyConversion = responseEntity.getBody();
+    BigDecimal conversionMultiple = currencyConversion.getConversionMultiple();
+
+    return new CurrencyConversion(10001L, from, to, quantity, conversionMultiple, quantity.multiply(conversionMultiple), "");
   }
 }
