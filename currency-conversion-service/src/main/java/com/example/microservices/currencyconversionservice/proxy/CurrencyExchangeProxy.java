969a912b937070189802f148a6ee34dceca402c6
Lucas Calebe <lucascalebe07@hotmail.com>
2022-11-15 11:55:14 -0300

    :wrench: setup feign proxy

---
+++ currency-conversion-service/src/main/java/com/example/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -0,0 +1,13 @@
+package com.example.microservices.currencyconversionservice.proxy;
+
+import com.example.microservices.currencyconversionservice.CurrencyConversion;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@FeignClient(name = "currency-exchange", url = "localhost:8000")
+public interface CurrencyExchangeProxy {
+
+  @GetMapping("/currency-exchange/from/{from}/to/{to}")
+  CurrencyConversion retrieveExchangeValue(@PathVariable String from, @PathVariable String to);
+}
