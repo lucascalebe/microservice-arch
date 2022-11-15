c6cf6a38f0c6c52c7ef2be1ae7e9e12b62fa742d
Lucas Calebe <lucascalebe07@hotmail.com>
2022-11-15 18:24:41 -0300

    :sparkles: add retry default 3 times to endpoint

--- currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/example/microservices/currencyexchangeservice/controller/CircuitBreakerController.java
@@ -1,13 +1,23 @@
 package com.example.microservices.currencyexchangeservice.controller;
 
+import io.github.resilience4j.retry.annotation.Retry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class CircuitBreakerController {
 
+  private final Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
+
   @GetMapping("/sample-api")
+  @Retry(name = "default")
   public String sampleApi() {
-    return "Sample API";
+    logger.info("Sample api call received");
+    var response = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-url", String.class);
+
+    return response.getBody();
   }
 }
